6109529c3a378ac2cdd056d2be1716ea6040fc5e
Ian Heraty <heratyian@gmail.com>
2023-07-25 19:01:43 +0000

    Hookup ipstack lookup

--- app.rb
+++ app.rb
@@ -4,9 +4,7 @@ require "./services/ipstack"
 
 get("/") do
   ip_address = request.env.dig("HTTP_X_REAL_IP")
-
-  # fetch info on ip address
-  @response = IPStack.lookup(ip_address)
+  @ip_lookup = IPStack.lookup(ip_address)
 
   erb(:root)
 end
--- services/ipstack.rb
+++ services/ipstack.rb
@@ -4,8 +4,7 @@ require "json"
 class IPStack
   def self.lookup(ip_address)
     response = HTTP.get("http://api.ipstack.com/#{ip_address}?access_key=#{ENV.fetch("IPSTACK_ACCESS_KEY")}")
-    parsed = JSON.parse(response)
 
-    parsed
+    JSON.parse(response.to_s)
   end
 end
