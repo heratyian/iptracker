46a8dc73681526c3b7b937c7bbb9ac8ccfcdacd5
Ian Heraty <heratyian@gmail.com>
2023-07-25 19:19:41 +0000

    Add error handling in IPStack lookup service.

--- services/ipstack.rb
+++ services/ipstack.rb
@@ -5,6 +5,6 @@ class IPStack
   def self.lookup(ip_address)
     response = HTTP.get("http://api.ipstack.com/#{ip_address}?access_key=#{ENV.fetch("IPSTACK_ACCESS_KEY")}")
 
-    JSON.parse(response.to_s)
+    response.success? ? JSON.parse(response.to_s) : {}
   end
 end
