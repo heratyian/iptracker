f78575cf5b3418d3151eb8cce1111823a39dff53
Ian Heraty <heratyian@gmail.com>
2023-07-25 19:27:05 +0000

    Change how we access the ip address.

--- app.rb
+++ app.rb
@@ -3,8 +3,7 @@ require "sinatra/reloader"
 require "./services/ipstack"
 
 get("/") do
-  ip_address = request.env.dig("HTTP_X_REAL_IP")
-  @ip_lookup = IPStack.lookup(ip_address)
+  @ip_lookup = IPStack.lookup(request.ip)
 
   erb(:root)
 end
